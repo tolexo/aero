76cdb54f2f3516c9e9619c41dee3cbb684df4424
Harsh <hkundra@gmail.com>
2015-06-11 22:33:06 +0530

    If can now take variable number of params

--- panik/panik.go
+++ panik/panik.go
@@ -1,13 +1,22 @@
 package panik
 
+import (
+	"fmt"
+)
+
 func On(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func If(condition bool, message string) {
+func If(condition bool, message string, params ...interface{}) {
 	if condition {
-		panic(message)
+		s := fmt.Sprintf(message, params...)
+		panic(s)
 	}
+}
+
+func Do(message string, params ...interface{}) {
+	If(true, message, params...)
 }
