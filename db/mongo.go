0caa84fa21fc8c1ee93a2313fb2c2e8e021606c9
mayur-tolexo <mayur.das@tolexo.com>
2016-08-04 19:12:01 +0530

    CLA-137 mongo adapter added

---
+++ db/mongo.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+	"github.com/tolexo/aero/conf"
+	"gopkg.in/mgo.v2"
+	"time"
+)
+
+// create the mongo connection string
+func getMongoConnStr(container string) (string, string) {
+	username := conf.String(container+".username", "")
+	password := conf.String(container+".password", "")
+	host := conf.String(container+".host", "")
+	port := conf.String(container+".port", "")
+	db := conf.String(container+".db", "")
+	replicas := conf.String(container+".replicas", "")
+	options := conf.String(container+".options", "")
+
+	if port != "" {
+		port = ":" + port
+	}
+	if replicas != "" {
+		replicas = "," + replicas
+	}
+	if options != "" {
+		options = "?" + options
+	}
+	auth := ""
+	if username != "" || password != "" {
+		auth = username + ":" + password + "@"
+	}
+
+	return fmt.Sprintf("mongodb://%s%s%s%s/%s%s", auth, host, port, replicas, db, options), db
+}
+
+// validate the container string
+func validateContainer(container ...string) (db string, err error) {
+	cLen := len(container)
+	if cLen == 0 {
+		container = append(container, "database.mongo")
+	} else if cLen > 1 {
+		err = errors.New("More than one container not supported")
+		return
+	}
+	if conf.Exists(container[0]) == false {
+		err = errors.New("mongo configuration missing")
+		return
+	}
+	db = container[0]
+	return
+}
+
+// create mongo connection
+func GetMongoConn(container ...string) (sess *mgo.Session, mdb string, err error) {
+	var db string
+	if db, err = validateContainer(container); err == nil {
+		conn, mdb := getMongoConnStr(db)
+		if mdb == "" {
+			err = errors.New("mongo database name missing")
+		}
+		sess, err = mgo.Dial(conn)
+		sess.SetSyncTimeout(10 * time.Minute)
+		sess.SetSocketTimeout(30 * time.Minute)
+		sess.SetCursorTimeout(0)
+	}
+	return
+}
