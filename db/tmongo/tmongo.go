833f7c83c7574e63d45fdbac6d48d8ef74e4df41
dhawal-tolexo <dhawal.arora@tolexo.com>
2016-10-06 19:00:37 +0530

    OptionalMgoT added structure for MgoTimeout

--- db/tmongo/tmongo.go
+++ db/tmongo/tmongo.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"github.com/tolexo/aero/conf"
 	"gopkg.in/mgo.v2"
-	"reflect"
 	"time"
 )
 
-const (
-	MGO_TIMEOUT = "timeout"
-)
+type MgoOption struct {
+	Timeout time.Duration
+}
 
 // create the mongo connection string
 func getMongoConnStr(container string) (string, string) {
@@ -53,7 +52,7 @@ func validateContainer(container string) (db string, err error) {
 
 // create mongo connection
 // TODO introduce parameter for connection additional settings like socket timeout
-func GetMongoConn(container string, param ...map[string]interface{}) (sess *mgo.Session, mdb string, err error) {
+func GetMongoConn(container string, param ...MgoOption) (sess *mgo.Session, mdb string, err error) {
 	var db string
 	if db, err = validateContainer(container); err == nil {
 		var conn string
@@ -63,18 +62,14 @@ func GetMongoConn(container string, param ...map[string]interface{}) (sess *mgo.
 			return
 		}
 		pLen := len(param)
-		if pLen > 1 {
-			err = errors.New("Parameters other than the Map not supported")
-		} else if pLen == 1 {
-			if tValue, tExist := param[0][MGO_TIMEOUT]; tExist == true {
-				if reflect.TypeOf(tValue).Kind() == reflect.Int64 {
-					sess, err = mgo.DialWithTimeout(conn, param[0][MGO_TIMEOUT].(time.Duration))
-				} else {
-					err = errors.New("Invalid Timeout Value")
-				}
+		if pLen == 1 {
+			if param[0].Timeout != time.Duration(0) {
+				sess, err = mgo.DialWithTimeout(conn, param[0].Timeout)
 			} else {
 				sess, err = mgo.Dial(conn)
 			}
+		} else if pLen > 1 {
+			err = errors.New("More than one MgoOption structures not supported")
 		} else {
 			sess, err = mgo.Dial(conn)
 		}
