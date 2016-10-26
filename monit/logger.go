483d711f25918915063387d66ceedec062c73b9f
dhawal-tolexo <dhawal.arora@tolexo.com>
2016-10-26 14:54:41 +0530

    TTA-226 added logger file for logging panics

---
+++ monit/logger.go
@@ -0,0 +1,26 @@
+package monit
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tolexo/aero/conf"
+)
+
+func PanicLogger(panicMsg interface{}) {
+	var (
+		logFp   *os.File
+		fileErr error
+		logger  *log.Logger
+	)
+	path := conf.String("logs.panic_log", "panic_log")
+	path = fmt.Sprintf("%s.log", path)
+	if logFp, fileErr = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); fileErr == nil {
+		logger = log.New(logFp, "panic", log.Lshortfile)
+		logger.Panic(panicMsg)
+	} else {
+		fmt.Println("Could not create the panic log file")
+		panic(panicMsg)
+	}
+}
