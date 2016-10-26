a59b2e67b506366909d57b256d36fdc7fb2a2484
dhawal-tolexo <dhawal.arora@tolexo.com>
2016-10-26 18:13:07 +0530

    TTA-226 logging stack trace

--- monit/logger.go
+++ monit/logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime/debug"
 	"time"
 
 	"github.com/tolexo/aero/conf"
@@ -19,7 +20,9 @@ func PanicLogger(panicMsg interface{}) {
 	path := conf.String("logs.panic_log", "panic_log")
 	path = fmt.Sprintf("%s_%d-%d-%d.log", path, sTime.Day(), sTime.Month(), sTime.Year())
 	if logFp, fileErr = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); fileErr == nil {
+		defer logFp.Close()
 		logger = log.New(logFp, "panic", log.Lshortfile)
+		logger.Print(string(debug.Stack()))
 		logger.Panic(panicMsg)
 	} else {
 		fmt.Println("Could not create the panic log file")
