a33f531a46549741331f63e645ee053f48adc238
dhawal-tolexo <dhawal.arora@tolexo.com>
2016-10-26 19:51:32 +0530

    TTA-226 added panic file logging for every day

--- monit/logger.go
+++ monit/logger.go
@@ -12,20 +12,27 @@ import (
 
 func PanicLogger(panicMsg interface{}) {
 	var (
-		logFp   *os.File
-		fileErr error
-		logger  *log.Logger
+		logFp      *os.File
+		fileErr    error
+		logger     *log.Logger
+		prevDay    int
+		currentDay int
 	)
 	sTime := time.Now().UTC()
-	path := conf.String("logs.panic_log", "panic_log")
-	path = fmt.Sprintf("%s_%d-%d-%d.log", path, sTime.Day(), sTime.Month(), sTime.Year())
-	if logFp, fileErr = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); fileErr == nil {
-		defer logFp.Close()
-		logger = log.New(logFp, "panic", log.Lshortfile)
-		logger.Print(string(debug.Stack()))
-		logger.Panic(panicMsg)
-	} else {
-		fmt.Println("Could not create the panic log file")
-		panic(panicMsg)
+	currentDay = sTime.Day()
+	if currentDay != prevDay {
+		if prevDay != 0 {
+			logFp.Close()
+		}
+		path := conf.String("logs.panic_log", "panic_log")
+		path = fmt.Sprintf("%s_%d-%d-%d.log", path, sTime.Day(), sTime.Month(), sTime.Year())
+		if logFp, fileErr = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); fileErr != nil {
+			fmt.Println("Could not create the panic log file")
+			panic(panicMsg)
+		}
+		prevDay = currentDay
 	}
+	logger = log.New(logFp, "panic", log.Lshortfile)
+	logger.Print(string(debug.Stack()))
+	logger.Panic(panicMsg)
 }
