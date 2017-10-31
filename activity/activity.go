f648158400369f4ffc6cbacc7f6c1e9e02b0e364
mayur-tolexo <mayur.das@tolexo.com>
2017-10-31 12:17:13 +0530

    PRA-410A: activity db change

--- activity/activity.go
+++ activity/activity.go
@@ -1,30 +1,32 @@
 package activity
 
 import (
-	"time"
-
+	"fmt"
 	"github.com/tolexo/aero/activity/model"
 	"github.com/tolexo/aero/db/tmongo"
-	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2"
+	"time"
 )
 
 const (
-	DB_CONTAINER = "database.omni"
+	DB_CONTAINER = "database.activity"
 )
 
 //Log User activity
 func LogActivity(url string, body interface{},
 	resp interface{}, respCode int, respTime float64) {
+	sTime := time.Now()
 	apiDetail := model.APIDetail{
 		Url:      url,
 		Body:     body,
 		Resp:     resp,
 		RespCode: respCode,
 		RespTime: respTime,
-		Time:     time.Now(),
+		Time:     sTime,
 	}
 	if sess, mdb, err := tmongo.GetMongoConn(DB_CONTAINER); err == nil {
 		defer sess.Close()
+		mdb = fmt.Sprintf("%v_%v_%v_%v", mdb, sTime.Day(), sTime.Month(), sTime.Year())
 		sess.SetSafe(&mgo.Safe{W: 0})
 		sess.DB(mdb).C("activity").Insert(apiDetail)
 	}
