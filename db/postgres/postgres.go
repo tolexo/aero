f85476a178b8667fd7aadcd2b58ad5258856faa1
mayur-tolexo <mayur.das@tolexo.com>
2018-02-16 19:02:27 +0530

    PRA-QL: query logger added

--- db/postgres/postgres.go
+++ db/postgres/postgres.go
@@ -2,12 +2,9 @@ package postgres
 
 import (
 	"errors"
-	"log"
+	"fmt"
 	"math/rand"
-	"reflect"
-	"runtime"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/go-pg/pg"
@@ -15,36 +12,26 @@ import (
 )
 
 var (
-	dbPostgresWrite *pg.DB
-	dbPostgresRead  []*pg.DB
-	readDebug       Debug
-	writeDebug      Debug
-	QL              *QueryLogger
-	MasterContainer = "database.master"
-	SlaveContainer  = "database.slaves"
+	isDebuggerActive bool
+	StartLogging     bool
+	dbPostgresWrite  *pg.DB
+	dbPostgresRead   []*pg.DB
+	MasterContainer  = "database.master"
+	SlaveContainer   = "database.slaves"
 )
 
 const (
 	GO_PG_PACKAGE = "/github.com/go-pg"
 )
 
-//Query log model contains method names and mux for locking
-type QueryLogger struct {
-	methodName map[string]bool
-	mux        sync.Mutex
-}
-
-//Debugger model contains DBConn on which query log will be created
-type Debug struct {
-	DBConn *pg.DB
-}
-
 //init master connection
 func initMaster() (err error) {
 	if conf.Exists(MasterContainer) {
 		if dbPostgresWrite != nil {
+			isDebuggerActive = true
 			return
 		} else {
+			isDebuggerActive = false
 			var postgresWriteOption pg.Options
 			if postgresWriteOption, err = getPostgresOptions(MasterContainer); err == nil {
 				dbPostgresWrite = pg.Connect(&postgresWriteOption)
@@ -61,7 +48,10 @@ func initSlaves() (err error) {
 	if conf.Exists(SlaveContainer) {
 		slaves := conf.StringSlice(SlaveContainer, []string{})
 		if dbPostgresRead == nil {
+			isDebuggerActive = false
 			dbPostgresRead = make([]*pg.DB, len(slaves))
+		} else {
+			isDebuggerActive = true
 		}
 		for i, container := range slaves {
 			if dbPostgresRead[i] == nil {
@@ -132,34 +122,21 @@ func getPostgresOptions(container string) (pgOption pg.Options, err error) {
 	return
 }
 
-//Start query logging
-func startQueryLog(debug *Debug, dbConn *pg.DB) {
-	if debug == nil {
-		debug = &Debug{DBConn: dbConn}
-		debug.LogQuery()
-	} else if reflect.DeepEqual(debug.DBConn, dbConn) == false {
-		debug.DBConn = dbConn
-		debug.LogQuery()
-	}
-}
-
 //Get postgres connection
 func Conn(writable bool) (dbConn *pg.DB, err error) {
 	if writable {
 		err = initMaster()
 		dbConn = dbPostgresWrite
-		startQueryLog(&writeDebug, dbConn)
 	} else {
 		err = initSlaves()
-		if dbPostgresRead == nil || len(dbPostgresRead) == 0 {
+		if len(dbPostgresRead) == 0 {
 			err = initMaster()
 			dbConn = dbPostgresWrite
-			startQueryLog(&writeDebug, dbConn)
 		} else {
 			dbConn = dbPostgresRead[rand.Intn(len(dbPostgresRead))]
-			startQueryLog(&readDebug, dbConn)
 		}
 	}
+	logQuery(dbConn)
 	return
 }
 
@@ -175,77 +152,24 @@ func ConnByContainer(container string) (*pg.DB, error) {
 	return nil, errors.New("No master or slaves container found in: " + container)
 }
 
-//Print postgresql query on terminal
-func (d *Debug) LogQuery() {
-	d.DBConn.OnQueryProcessed(func(event *pg.QueryProcessedEvent) {
-		if conf.Bool("debug_query.log_query", false) == true && QL != nil {
-			methodName := methodName(4)
-			if QL.methodName[methodName] == true {
+//logQuery : Print postgresql query on terminal
+func logQuery(conn *pg.DB) {
+	if isDebuggerActive == false {
+		isDebuggerActive = true
+		conn.OnQueryProcessed(func(event *pg.QueryProcessedEvent) {
+			if StartLogging == true {
 				if query, err := event.FormattedQuery(); err == nil {
 					var queryError string
 					if event.Error != nil {
 						queryError = "\nQUERY ERROR: " + event.Error.Error()
 					}
-					log.Printf("\nFile: %v : %v\nFunction: %v\nQuery Execution Taken: %s\n%s%s\n\n",
+					fmt.Println("----DEBUGGER----")
+					fmt.Printf("\nFile: %v : %v\nFunction: %v\nQuery Execution Taken: %s\n%s%s\n\n",
 						event.File, event.Line, event.Func, time.Since(event.StartTime), query, queryError)
 				} else {
-					log.Println("LogQuery Error: " + err.Error())
+					fmt.Println("Debugger Error: " + err.Error())
 				}
 			}
-		}
-	})
-}
-
-//Get method name of function caller
-func methodName(depth int) (method string) {
-	for i := depth; ; i++ {
-		pc, file, _, ok := runtime.Caller(i)
-		if ok == false {
-			break
-		}
-		if strings.Contains(file, GO_PG_PACKAGE) {
-			continue
-		}
-		methodName := pkgMethod(pc)
-		if ind := strings.Index(methodName, "."); ind > 0 {
-			method = methodName[ind+1:]
-			break
-		}
+		})
 	}
-	return
-}
-
-//Get package method name and method pointer name from program counter
-func pkgMethod(pc uintptr) (method string) {
-	if f := runtime.FuncForPC(pc); f != nil {
-		method = f.Name()
-		if ind := strings.LastIndex(method, "/"); ind > 0 {
-			method = method[ind+1:]
-		}
-		if ind := strings.Index(method, "."); ind > 0 {
-			method = method[ind+1:]
-		}
-	}
-	return
-}
-
-//Add methodName in queryLogger model
-func (q *QueryLogger) AddMethod(methodName string) {
-	q.mux.Lock()
-	q.methodName[methodName] = true
-	q.mux.Unlock()
-}
-
-//Remove methodName from queryLogger model
-func (q *QueryLogger) RemoveMethod(methodName string) {
-	q.mux.Lock()
-	if _, exists := q.methodName[methodName]; exists == true {
-		delete(q.methodName, methodName)
-	}
-	q.mux.Unlock()
-}
-
-//Create new object of query logger
-func NewQueryLogger() {
-	QL = &QueryLogger{methodName: make(map[string]bool)}
 }
