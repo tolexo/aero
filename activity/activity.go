16d957eaf85ea850aff05c8619b3c7a336893040
mayur-tolexo <mayur.das@tolexo.com>
2018-03-16 11:18:17 +0530

    ELog: error logger function added in activity

--- activity/activity.go
+++ activity/activity.go
@@ -2,10 +2,15 @@ package activity
 
 import (
 	"fmt"
-	"github.com/tolexo/aero/activity/model"
-	"github.com/tolexo/aero/db/tmongo"
-	"gopkg.in/mgo.v2"
+	"log"
+	"os"
 	"time"
+
+	"github.com/tolexo/aero/activity/model"
+	"github.com/tolexo/aero/conf"
+	"github.com/tolexo/aero/db/tmongo"
+	"github.com/tolexo/aero/panik"
+	"gopkg.in/mgo.v2"
 )
 
 const (
@@ -31,4 +36,19 @@ func LogActivity(url string, serviceID string, body interface{},
 		sess.SetSafe(&mgo.Safe{W: 0})
 		sess.DB(mdb).C("activity").Insert(apiDetail)
 	}
+}
+
+//LogCSV : will log in csv
+func LogCSV(serviceID, url string, respTime float64, respCode int64) {
+	go func() {
+		logfile := conf.String("monitor.filelog", "")
+		if logfile != "" {
+			t := time.Now()
+			logfileSuffix := fmt.Sprintf("%d-%d-%d", t.Month(), t.Day(), t.Hour())
+			f, err := os.OpenFile(logfile+logfileSuffix+".csv", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+			panik.On(err)
+			l := log.New(f, "", log.LstdFlags)
+			l.Printf("%s %s %f %d", serviceID, url, respTime, respCode)
+		}
+	}()
 }
